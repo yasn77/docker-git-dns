a3fb75adf1b02b095a6281fc62f4e201a504d43f
Yasser Nabi <yassersaleemi@gmail.com>
2015-02-01 21:53:04 +0000

    WIP: Updated Python script some more
    Incremental changes to a working script

--- git_bind_update.py
+++ git_bind_update.py
@@ -26,8 +26,11 @@ def get_config():
         if k in os.environ.keys():
             config[v] = os.environ[k]
     # Set some defaults
-    config['config_path'] = config['config_path'] if not config.has_key('config_path') else None
-    config['update'] = config['update'] if not config.has_key('update') else '30m'
+    if not config.has_key('config_path'):
+        config['config_path'] = None
+    if not config.has_key('update'):
+        config['update'] = '30m'
+    config['repo_dir'] = repo_dir
     return config
 
 def __log(msg):
@@ -44,11 +47,32 @@ def to_sec(u, v):
         return int(v)
 
 def clone_repo():
-    repo = Repo.clone_from(config['repo'], repo_dir)
+    __log("Cloning repository : {0}".format(config['repo_dir']))
+    repo = Repo.clone_from(config['repo'], config['repo_dir'])
     return repo
 
+def has_update(repo):
+    pass
+
 def main():
-    repo = clone_repo
+    repo = clone_repo()
+    unit = config['update'][-1].lower()
+    if unit.isalpha():
+        interval = config['update'][0:-1]
+    else:
+        interval = config['update']
+    try:
+        sec = to_sec(unit, interval)
+    except ValueError:
+        __log('ERR: Update interval is set to: {0}'.format(config['update']))
+        __log('ERR: Problem trying to convert {0} to seconds...exiting'.format(interval))
+        exit(1)
+    __log("Going in to run loop")
+    while True:
+        if has_update(repo):
+            pass
+    __log("Sleeping for {0} sec".format(sec))
+    time.sleep(sec)
 
 if __name__ == '__main__':
     config = get_config()
